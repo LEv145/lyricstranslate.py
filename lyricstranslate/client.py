22959fa2c861d488b67d3bf228f6efd182ae9c9e
LEv145 <levxxx03@gmail.com>
2022-01-28 23:46:50 +0200

    Add `client`

--- lyricstranslate/client.py
+++ lyricstranslate/client.py
@@ -1,5 +1,13 @@
+from typing import (
+    List,
+)
+
 from .api import LyricsTranslateAPI
-from .converter import Converter
+from .converter import (
+    Converter,
+    Suggestion,
+    TrackHTMLResult,
+)
 
 
 class LyricsTranslateClient():
@@ -9,5 +17,12 @@ class LyricsTranslateClient():
         converter: Converter,
     ):
         self._api = api
-        self.converter = converter
+        self._converter = converter
 
+    async def search(self, query: str) -> List[Suggestion]:
+        result = await self._api.search(query)
+        return self._converter.convert_search_response(result)
+
+    async def get_song_by_url(self, url: str) -> TrackHTMLResult:
+        result = await self._api.get_song_by_url(url)
+        return self._converter.convert_song_html_response(result)
